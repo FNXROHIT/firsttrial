413dc241074493db6a593b8ad60c320d1cdfdefa
Rohit <rohitsinghal1997@gmail.com>
2021-01-17 11:41:21 +0530

    newone

---
+++ force-app/main/default/lwc/emloyeeInfo/emloyeeInfo.js
@@ -0,0 +1,33 @@
+import { LightningElement, track } from 'lwc';
+
+export default class EmloyeeInfo extends LightningElement {
+    @track name;
+    @track age;
+    @track salary;
+    @track show=true;
+    //hello 1
+
+
+    nameHandler(event)
+    {
+        this.name = event.target.value;
+
+    }
+
+    ageHandler(event)
+    {
+        this.age = event.target.value;
+        
+    }
+
+    salaryHandler(event)
+    {
+        this.salary = event.target.value;
+        
+    }
+
+    saveButtonHandler(){
+        this.show=false;
+    }
+
+}
